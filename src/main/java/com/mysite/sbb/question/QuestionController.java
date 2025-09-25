a502f3d76e9bb62f9fff841897cff54fab6b74a1
junyeonggoodboy <76607722+junyeonggoodboy@users.noreply.github.com>
2025-09-26 01:41:27 +0900

    commit

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,27 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-    private final QuestionServer questionServer;
-    @GetMapping("/question/list")
+    private final QuestionService questionService;
+    @GetMapping("/list")
     public String list(Model model){
-        List<Question> questionList = this.questionServer.getList();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
     }
 
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
-        Question question = this.questionServer.getQuestion(id);
+        Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
